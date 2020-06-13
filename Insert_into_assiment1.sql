b35d32e0f7f8f1fb4d55ea4d761c3ea159c2b105
kienvt14 <trungkien140420011@gmail.com>
2020-06-13 22:36:31 +0700

    Insert data Version 1

---
+++ Insert_into_assiment1.sql
@@ -0,0 +1,228 @@
+USE LibraryAssiment
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD312' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD311' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD315' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD318' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD302' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD305' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD308' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD307' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD306' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD320' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD3040' -- Name_Class - varchar
+)	
+INSERT	dbo.Class
+(
+    --Id_Class - column value is auto-generated
+    Name_Class
+)
+VALUES
+(
+    -- Id_Class - int
+    'GCD3030' -- Name_Class - varchar
+)	
+SELECT	c.* FROM dbo.Class c	
+
+
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Regina Montoya','Feb 3, 2000',8),('Thane Stephenson','Mar 15, 2001',8),('Sybil Keith','Mar 10, 1999',4),('Amethyst Padilla','Jun 18, 1997',4),('Rina Glass','May 18, 1996',4),('Quinn Stephens','Apr 18, 1998',4),('Clarke Gray','Jan 2, 1996',6),('Harriet Duke','Feb 23, 1998',5),('Len Whitfield','Sep 15, 2000',6),('Benjamin Simmons','Jun 6, 1998',5);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Christen Johnston','Feb 7, 2000',2),('Quinn Duran','Feb 22, 2002',6),('Brandon Campbell','Mar 6, 1997',1),('Anjolie Mendez','Mar 3, 1997',3),('Gisela Petersen','Feb 1, 1999',11),('Kimberley Chase','Apr 7, 2002',3),('Tatyana Humphrey','May 7, 2000',5),('Fay Kaufman','Jun 5, 1995',11),('Cassady Page','Oct 21, 1999',5),('Steven Emerson','Oct 24, 1997',1);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Prescott Graham','Feb 5, 2002',7),('Kareem Jackson','Jun 21, 1996',10),('Mary Griffin','Nov 26, 1998',8),('Devin Ellis','Nov 27, 1999',10),('Abigail Myers','Mar 7, 1997',10),('Mercedes Gates','Dec 27, 1997',1),('Teagan Brady','Jan 31, 1998',7),('Moana Baldwin','Sep 17, 1999',2),('Kimberly Dunn','Feb 2, 1996',2),('Avram Holder','Aug 23, 1996',5);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Idona Cortez','Oct 16, 1999',10),('Yvonne Tanner','Aug 17, 1995',7),('Medge Gray','Aug 21, 1998',1),('Emerson Munoz','Jun 8, 1998',4),('Kyle Church','Jun 2, 2002',4),('Rahim Bartlett','Jun 10, 1996',3),('Courtney Burch','Sep 25, 1997',4),('Steven Juarez','Aug 14, 1996',4),('Shellie Case','Jun 3, 1997',9),('Dexter Bradley','May 28, 1999',10);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Taylor Joseph','Mar 29, 2002',1),('Mara Mcfarland','Oct 7, 2001',7),('Vernon Obrien','May 23, 2000',3),('Francis Wheeler','Oct 17, 1997',1),('Nero Barnes','Sep 4, 2000',11),('Carson Thomas','Jun 10, 2001',7),('Bernard Melton','Oct 1, 1999',1),('Ruby Fowler','Jan 27, 1998',4),('Latifah French','Jul 21, 1998',8),('Minerva Mayer','Aug 14, 2000',8);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Vivien Weaver','Sep 22, 1995',5),('Jordan Myers','Nov 17, 1997',8),('Brian Roth','Feb 13, 1996',7),('Hadassah Phelps','Jul 26, 1998',6),('Lara Mcintyre','Oct 7, 1996',7),('Reagan Webster','Jan 30, 1996',5),('Deborah Terrell','May 9, 2001',7),('Margaret Peterson','Sep 5, 2001',3),('August Williams','Aug 22, 1999',8),('Felix Fuentes','Jan 2, 2001',4);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Jameson Farley','Feb 18, 2002',1),('September Jacobson','Sep 28, 1996',4),('Maia Sims','Dec 6, 1995',2),('Xaviera Meyers','Jun 21, 1995',10),('Lee Navarro','Jul 14, 1995',6),('Ignacia Chandler','Aug 27, 1998',2),('Orlando Peck','May 13, 2002',9),('Nolan Witt','Aug 12, 2001',4),('Ronan Pollard','Jun 25, 2000',6),('Rigel Glass','Oct 18, 2001',7);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Colt Bell','Feb 9, 1996',10),('Kelsie Ball','Feb 3, 1999',5),('Diana Bruce','Dec 21, 2001',5),('George Burks','Oct 16, 1998',6),('Ray Robertson','Apr 19, 2001',6),('Lionel Garza','Dec 11, 1996',2),('Alyssa Rasmussen','Mar 28, 1997',6),('Sebastian Hodge','Sep 22, 1996',10),('Bell Beck','Nov 19, 2001',2),('Kimberley Parks','Jun 14, 1999',3);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Tyrone Silva','Jun 22, 1998',8),('Cadman Stanley','Jun 24, 1996',4),('Ingrid Schneider','Sep 24, 1998',2),('Brenda Valenzuela','Aug 26, 1998',7),('Laura Riggs','Jan 20, 2002',3),('Zoe Myers','Jan 9, 2001',10),('Ora Mcconnell','Feb 12, 1996',7),('Melvin Raymond','Mar 28, 1996',5),('Seth Bruce','Dec 1, 2000',1),('Colette Deleon','Feb 16, 2000',11);
+INSERT INTO student([FullName],[Birthdate],[Id_Class]) VALUES('Allen Wyatt','Oct 29, 1996',11),('Idola Pittman','Jun 18, 2000',9),('Fletcher Gross','May 7, 2000',10),('Ashely Harmon','Sep 18, 1996',4),('Thomas Wolfe','Nov 22, 2001',7),('Chanda Dillon','Jun 11, 1995',9),('Garth Rose','Mar 25, 1996',1),('Astra Leonard','Aug 23, 2001',6),('Blaine Reed','Jan 18, 2000',5),('Lisandra Franco','Jun 14, 1997',6);
+
+SELECT	s.* FROM dbo.student s	
+
+INSERT INTO Author([Name_author]) VALUES('Nguyen Hoang Lam'),('Bui Xuan Huan'),('Kha Banh'),('Vo Ngoc Thanh'),('Xemesis'),('ViruSs'),('Pew Pew'),('Mixi'),('Phan Van Teo'),('Newman');
+
+SELECT	a.* FROM dbo.Author a
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'Mystery' -- Type_book - varchar
+)	
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'History' -- Type_book - varchar
+)	
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'Journal' -- Type_book - varchar
+)	
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'Fantasy' -- Type_book - varchar
+)	
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'Thriller' -- Type_book - varchar
+)	
+INSERT	INTO	dbo.Type_Book
+(
+    --Id_type - column value is auto-generated
+    Type_book
+)
+VALUES
+(
+    -- Id_type - int
+    'Science' -- Type_book - varchar
+)	
+SELECT	tb.* FROM dbo.Type_Book tb	
+
+
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Sint-Ulriks-Kapelle','$679.03','08/23/2020',3,10,2),('Cobourg','$842.87','09/30/2019',3,2,5),('Gijzegem','$654.45','09/24/2019',3,8,4),('Prenzlau','$603.26','03/09/2020',3,1,4),('South Bend','$811.11','05/21/2020',2,9,6),('Almería','$646.35','12/02/2020',1,3,5),('Charlottetown','$556.32','07/27/2019',4,8,5),('Ajax','$689.41','05/12/2020',4,2,1),('Bevagna','$564.64','04/23/2021',4,5,2),('Biez','$841.47','08/25/2020',3,8,6);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Osimo','$992.72','10/07/2019',4,3,3),('Chambord','$958.53','05/11/2021',4,10,3),('Giarratana','$827.13','12/07/2020',4,10,5),('Leoben','$822.94','10/28/2019',2,1,4),('Eindhoven','$680.83','01/23/2021',2,9,6),('Anápolis','$546.76','09/03/2020',1,10,1),('Lichfield','$563.39','09/28/2020',3,4,1),('Etobicoke','$892.76','07/13/2020',1,3,5),('Peine','$761.31','09/09/2020',5,5,1),('Longueville','$799.35','07/18/2019',2,6,6);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Wakefield','$591.37','05/07/2021',5,9,2),('Aalst','$666.83','03/13/2021',5,2,4),('Padre las Casas','$628.69','11/29/2020',1,3,5),('Nieuwkapelle','$807.20','06/13/2019',1,6,1),('Cariboo Regional District','$556.29','04/29/2020',3,1,4),('Boston','$550.70','04/29/2021',1,2,5),('Lafayette','$918.44','06/10/2021',5,9,2),('Santa Coloma de Gramenet','$555.41','04/17/2020',4,3,3),('Zelzate','$941.95','05/26/2021',5,3,5),('Aquila d''Arroscia','$843.43','04/14/2021',4,8,2);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Hinckley','$996.52','03/08/2021',2,7,6),('Lieferinge','$594.81','01/10/2021',2,2,6),('Märsta','$717.33','10/09/2020',3,3,1),('Huntsville','$760.70','06/20/2020',4,10,1),('Armadale','$704.11','05/08/2021',1,2,4),('Greenock','$554.26','08/24/2020',1,9,3),('Bokaro Steel City','$996.90','01/17/2021',5,4,3),('Saint-Pierre','$966.10','09/16/2020',5,7,4),('Brussel','$539.75','07/12/2020',4,8,3),('Pimpri-Chinchwad','$541.67','08/14/2019',5,3,1);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Gießen','$608.81','04/08/2021',5,10,3),('Wibrin','$800.16','02/15/2021',3,7,3),('Cagnes-sur-Mer','$680.62','03/14/2020',4,2,1),('Cantley','$765.84','04/22/2020',3,5,5),('North Saanich','$622.21','11/15/2019',5,5,1),('Coleville Lake','$610.61','04/23/2020',4,2,2),('Pickering','$541.21','02/10/2021',3,3,4),('Calmar','$552.21','09/18/2020',3,5,2),('Katihar','$867.49','09/23/2019',3,4,2),('Raychikhinsk','$798.12','04/16/2020',4,4,1);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Dijon','$609.75','11/20/2020',3,1,6),('Challand-Saint-Victor','$663.37','03/02/2020',4,2,4),('Gboko','$636.99','12/06/2019',3,10,4),('Chelsea','$829.18','04/04/2021',2,9,1),('Shipshaw','$515.30','12/09/2020',5,5,5),('Berg','$770.16','11/26/2020',1,7,1),('Motherwell','$732.17','12/16/2020',5,2,6),('Zwijnaarde','$792.28','10/16/2019',5,4,1),('Pickering','$788.07','02/25/2021',2,1,4),('Saint-Urbain','$616.86','11/17/2019',5,4,2);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Asbestos','$518.89','12/20/2020',4,4,6),('Suncheon','$732.20','11/21/2020',2,9,1),('Tarrasa','$698.77','06/05/2021',1,7,6),('Bielefeld','$602.01','07/30/2019',4,2,1),('Marchihue','$916.58','04/26/2021',4,2,6),('Talence','$905.68','07/28/2020',1,8,2),('Carleton','$881.98','02/12/2020',5,5,1),('Zaraysk','$603.31','02/09/2021',5,2,6),('Hudson''s Hope','$582.07','02/17/2020',5,7,5),('Montignoso','$621.47','07/31/2020',2,8,4);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Châlons-en-Champagne','$778.37','08/08/2020',3,2,3),('Westmalle','$725.74','11/05/2019',5,8,2),('Silverton','$969.64','08/14/2020',5,2,5),('Cuglieri','$567.18','04/11/2021',5,3,4),('Cork','$756.90','05/16/2020',4,3,4),('Fort Wayne','$632.33','12/30/2019',3,10,2),('Mattersburg','$809.74','07/21/2019',5,5,6),('Harlingen','$543.57','10/29/2019',5,3,4),('Thorold','$967.37','06/11/2021',5,8,5),('Soacha','$852.13','12/18/2019',2,2,4);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Tirúa','$649.85','05/29/2020',4,5,4),('Orciano Pisano','$510.20','09/22/2020',4,2,5),('Afsnee','$636.87','04/02/2021',3,1,4),('Roxburgh','$893.50','04/01/2020',2,5,1),('Oviedo','$815.47','03/10/2021',3,7,3),('Township of Minden Hills','$786.45','03/20/2021',1,6,1),('Kumluca','$559.02','07/23/2019',3,8,5),('Amritsar','$790.25','05/24/2020',4,4,3),('Newport','$878.99','05/03/2020',3,8,1),('Casalvieri','$520.54','08/20/2020',5,10,3);
+INSERT INTO Books([Name_book],[price],[publication_date],[number_of_books],[Id_author],[Id_type]) VALUES('Perinaldo','$968.35','12/16/2019',4,7,3),('Nanton','$698.62','01/16/2020',4,9,5),('Bicinicco','$821.83','07/12/2019',5,3,1),('Burgos','$928.61','10/24/2019',4,5,1),('Loughborough','$840.14','01/29/2020',5,8,4),('Collipulli','$640.56','01/30/2021',5,2,3),('Périgueux','$739.17','04/02/2021',1,4,1),('Meugliano','$719.82','06/03/2020',4,5,5),('Ceyhan','$839.40','10/23/2020',3,9,1),('Houtave','$859.96','07/25/2019',5,2,2);
+
+SELECT	b.* FROM dbo.Books b
+SELECT	s.* FROM dbo.student s	
+
+
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(44,87,'Nov 12, 2019','Dec 13, 2020'),(81,12,'Oct 28, 2019','Apr 11, 2021'),(1,63,'Oct 23, 2019','Mar 4, 2021'),(69,51,'May 13, 2019','Nov 16, 2020'),(66,66,'May 11, 2019','Nov 7, 2020'),(73,54,'Apr 19, 2019','May 28, 2021'),(40,30,'Dec 9, 2019','Sep 19, 2020'),(23,44,'Jul 5, 2019','Jan 6, 2021'),(6,24,'Jul 21, 2019','Jan 28, 2021'),(3,35,'Apr 23, 2019','Mar 26, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(60,71,'May 14, 2019','Jan 10, 2021'),(64,34,'Nov 19, 2019','Mar 26, 2020'),(84,13,'Aug 28, 2019','Jan 17, 2021'),(51,22,'Sep 29, 2019','Feb 17, 2020'),(22,10,'Jun 21, 2019','Dec 24, 2020'),(2,65,'Oct 25, 2019','Apr 17, 2021'),(44,59,'Oct 29, 2019','Feb 26, 2021'),(37,76,'Jun 7, 2019','Apr 15, 2020'),(62,42,'Oct 20, 2019','Oct 4, 2020'),(69,19,'Jun 24, 2019','Jul 18, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(2,74,'Aug 26, 2019','Mar 24, 2021'),(62,76,'Nov 6, 2019','Nov 23, 2020'),(97,17,'Dec 9, 2019','Apr 24, 2021'),(5,92,'Sep 8, 2019','Dec 4, 2020'),(70,34,'Mar 24, 2019','May 21, 2020'),(16,29,'Feb 8, 2019','May 4, 2020'),(48,72,'Jun 25, 2019','Mar 13, 2021'),(55,40,'Jan 2, 2019','May 31, 2020'),(99,77,'Mar 31, 2019','Aug 24, 2020'),(98,72,'Sep 22, 2019','Mar 21, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(13,51,'Sep 23, 2019','Feb 12, 2021'),(40,50,'Jan 3, 2019','Oct 15, 2020'),(71,88,'Sep 20, 2019','Nov 11, 2020'),(13,92,'Aug 4, 2019','Mar 12, 2020'),(34,33,'Oct 22, 2019','Dec 17, 2020'),(9,71,'Jun 2, 2019','Sep 29, 2020'),(21,96,'Mar 26, 2019','Dec 11, 2020'),(55,77,'Oct 19, 2019','May 23, 2020'),(31,28,'Oct 7, 2019','Feb 2, 2021'),(46,26,'May 11, 2019','Feb 3, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(26,32,'Mar 31, 2019','Sep 25, 2020'),(72,60,'Nov 5, 2019','Nov 12, 2020'),(72,80,'Jun 11, 2019','Feb 16, 2021'),(11,72,'Oct 7, 2019','Aug 19, 2020'),(54,72,'Aug 28, 2019','Jan 4, 2021'),(1,75,'Jul 18, 2019','May 15, 2021'),(50,48,'Oct 24, 2019','Aug 18, 2020'),(42,78,'Mar 23, 2019','Apr 9, 2020'),(92,60,'Aug 24, 2019','Feb 3, 2021'),(1,59,'May 23, 2019','Jan 17, 2021');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(44,76,'Feb 16, 2019','Apr 11, 2021'),(25,44,'Oct 24, 2019','Jun 9, 2020'),(98,6,'Apr 13, 2019','Feb 5, 2021'),(22,19,'Mar 29, 2019','Feb 5, 2020'),(16,61,'Nov 25, 2019','May 8, 2020'),(80,5,'Nov 29, 2019','Dec 28, 2020'),(90,31,'Mar 12, 2019','Feb 13, 2021'),(67,30,'Jul 14, 2019','Aug 28, 2020'),(82,25,'Aug 1, 2019','May 27, 2020'),(28,91,'Jul 6, 2019','Jul 24, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(19,40,'Feb 9, 2019','Jan 1, 2021'),(11,93,'Aug 21, 2019','May 13, 2021'),(18,76,'Jul 2, 2019','Jun 7, 2020'),(87,78,'Apr 8, 2019','May 5, 2021'),(31,34,'Nov 19, 2019','Sep 10, 2020'),(99,48,'Sep 4, 2019','Jun 2, 2020'),(23,97,'Sep 16, 2019','Dec 18, 2020'),(70,32,'May 17, 2019','Jan 16, 2021'),(91,62,'Oct 26, 2019','Apr 19, 2021'),(50,47,'Apr 27, 2019','Jul 11, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(86,28,'Feb 10, 2019','May 17, 2020'),(92,33,'Apr 20, 2019','Feb 6, 2021'),(78,28,'Mar 14, 2019','Apr 27, 2020'),(91,48,'Feb 14, 2019','Jun 29, 2020'),(63,10,'May 23, 2019','Feb 12, 2020'),(92,40,'Dec 17, 2019','Apr 19, 2020'),(89,97,'Sep 9, 2019','Apr 7, 2020'),(69,48,'Dec 10, 2019','Jul 19, 2020'),(3,39,'Nov 6, 2019','Jul 28, 2020'),(52,42,'May 19, 2019','Jan 18, 2021');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(81,36,'May 26, 2019','Mar 6, 2020'),(80,7,'Dec 8, 2019','Nov 7, 2020'),(17,84,'Sep 4, 2019','Dec 1, 2020'),(19,9,'Apr 25, 2019','Dec 15, 2020'),(57,67,'Sep 21, 2019','May 9, 2020'),(92,78,'Oct 20, 2019','Oct 17, 2020'),(18,23,'Oct 30, 2019','Mar 10, 2021'),(31,72,'Sep 25, 2019','Jan 4, 2021'),(79,89,'May 30, 2019','Nov 20, 2020'),(31,20,'Feb 28, 2019','Sep 15, 2020');
+INSERT INTO Borrows([Id_student],[Id_Book],[takendate],[broughtdate]) VALUES(86,72,'May 7, 2019','Apr 17, 2021'),(43,87,'Nov 5, 2019','Apr 29, 2020'),(45,90,'Jun 17, 2019','May 27, 2020'),(25,3,'Mar 11, 2019','Jan 21, 2021'),(1,81,'Nov 15, 2019','Oct 1, 2020'),(25,81,'Jan 23, 2019','Mar 8, 2020'),(46,17,'Feb 13, 2019','May 25, 2021'),(93,68,'Jan 11, 2019','Apr 6, 2020'),(27,10,'Jan 26, 2019','Aug 20, 2020'),(22,33,'Feb 9, 2019','Dec 19, 2020');
